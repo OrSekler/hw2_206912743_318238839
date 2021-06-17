9398411779c8b2a7ae5ddc8af3a9ff67becb220c
wargame <or.sekler@campus.technion.ac.il>
2021-06-17 16:16:43 +0300

    Initial commit

---
+++ src/StorgeItem.java
@@ -0,0 +1,15 @@
+public class StorgeItem {
+    int itemSize;
+    String itemName;
+    private long itemCreationDate;
+
+    // constructor
+    public StorgeItem(String name){
+        this.itemName = name;
+        setDate();
+    }
+
+    public void setDate(){
+        this.itemCreationDate =
+    }
+}
